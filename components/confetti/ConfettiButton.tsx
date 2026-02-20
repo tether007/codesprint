a4d23986a72aad040e9d0e3d4000158eacd7fce3
tether007 <toshrosh2005@gmail.com>
2026-02-20 23:55:05 +0530

    confetti

--- components/confetti/ConfettiButton.tsx
+++ components/confetti/ConfettiButton.tsx
@@ -1,57 +1,57 @@
-'use client';
+// 'use client';
 
 
 
-// just afew import errors
-import { useRef } from 'react';
-import confetti from 'canvas-confetti';
-import type { ConfettiRef } from '../confetti
-import { Confetti } from '@/components/confetti';
+// // just afew import errors
+// import { useRef } from 'react';
+// import confetti from 'canvas-confetti';
+// import type { ConfettiRef } from '../confetti
+// import { Confetti } from '@/components/confetti';
 
-interface ConfettiButtonProps {
-  children: React.ReactNode;
-  options?: confetti.Options;
-  className?: string;
-}
+// interface ConfettiButtonProps {
+//   children: React.ReactNode;
+//   options?: confetti.Options;
+//   className?: string;
+// }
 
-export function ConfettiButton({
-  children,
-  options = {},
-  className = '',
-}: ConfettiButtonProps) {
-  const confettiRef = useRef<ConfettiRef>(null);
+// export function ConfettiButton({
+//   children,
+//   options = {},
+//   className = '',
+// }: ConfettiButtonProps) {
+//   const confettiRef = useRef<ConfettiRef>(null);
 
-  const handleClick = () => {
-    confettiRef.current?.fire(options);
-  };
+//   const handleClick = () => {
+//     confettiRef.current?.fire(options);
+//   };
 
-  return (
-    <div className="relative inline-block">
-      <button onClick={handleClick} className={className}>
-        {children}
-      </button>
-      <Confetti
-        ref={confettiRef}
-        className="pointer-events-none absolute left-0 top-0 z-50 size-full"
-      />
-    </div>
-  );
-}
+//   return (
+//     <div className="relative inline-block">
+//       <button onClick={handleClick} className={className}>
+//         {children}
+//       </button>
+//       <Confetti
+//         ref={confettiRef}
+//         className="pointer-events-none absolute left-0 top-0 z-50 size-full"
+//       />
+//     </div>
+//   );
+// }
 
-// Hook version for more control
-export function useConfettiButton() {
-  const confettiRef = useRef<ConfettiRef>(null);
+// // Hook version for more control
+// export function useConfettiButton() {
+//   const confettiRef = useRef<ConfettiRef>(null);
 
-  const fire = (options?: confetti.Options) => {
-    confettiRef.current?.fire(options || {});
-  };
+//   const fire = (options?: confetti.Options) => {
+//     confettiRef.current?.fire(options || {});
+//   };
 
-  const ConfettiComponent = () => (
-    <Confetti
-      ref={confettiRef}
-      className="pointer-events-none absolute left-0 top-0 z-50 size-full"
-    />
-  );
+//   const ConfettiComponent = () => (
+//     <Confetti
+//       ref={confettiRef}
+//       className="pointer-events-none absolute left-0 top-0 z-50 size-full"
+//     />
+//   );
 
-  return { fire, ConfettiComponent };
-}
+//   return { fire, ConfettiComponent };
+// }
